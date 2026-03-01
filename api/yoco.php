21acd310b60a16faf051ec87b7f03008213da086
Rualdo Kruger <kruger.rualdo@gmail.com>
2026-03-01 13:49:05 +0200

    Delete api directory

--- api/yoco.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Store your Yoco secret key in this file (keep it out of frontend bundles).
-// On the server, set an environment variable YOCO_SECRET_KEY, or define it here.
-// Preferred: set the env var in hosting; fallback: uncomment and set manually.
-
-// Example (uncomment and set if env vars aren’t available):
-$YOCO_SECRET_KEY = 'sk_live_a808d905OgakKe122d94b559de6e';
-
-// If an env var exists, it will be used by yoco-charge.php automatically.
